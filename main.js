fc17c5175557e0934cdf9526b997f8d0f710f831
James Wills <james@heebie.co.uk>
2022-02-16 20:01:26 +0000

    add compare DNA method which prints a message saying the percentage DNA shared by the two specimens

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Returns a random DNA base
-const dnaBases = ['A', 'T', 'C', 'G'];
+
 const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -13,6 +14,7 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// factory function creating the organism with 15 bases
 function pAequorFactory(number, baseArray) {
   return {
     specimenNum: number,
@@ -25,9 +27,23 @@ function pAequorFactory(number, baseArray) {
       }
       this.dna[selectedBaseIndex] = newBase;
     },
+    compareDNA: function (pAequorX) {
+      let matchingBases = [];
+      for (let i = 0; i < 15; i++) {
+        if (this['dna'][i] === pAequorX['dna'][i]) {
+          matchingBases.push(this['dna'][i]);
+        };
+        
+      }
+      console.log(`specimen ${this.specimenNum} and specimen ${pAequorX.specimenNum} have ${matchingBases.length / 15 *100}% DNA in common`);
+    },
   }
 }
 
+let pAequor1 = pAequorFactory(1, mockUpStrand());
+let pAequor2 = pAequorFactory(2, mockUpStrand());
 
-
+//console.log(pAequor1.dna);
+//console.log(pAequor2.dna);
+pAequor1.compareDNA(pAequor2);
 
